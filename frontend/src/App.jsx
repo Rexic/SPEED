8715a1bcfcc830a49399aff7fceabff6be1240f7
Rexic <55978892+Rexic@users.noreply.github.com>
2022-09-25 06:25:26 +1300

    implemented eslint (#1)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-        </div>
+        <div />
       </Router>
     );
   }
