b6a7aceeb63db861003af36f4912c4a73f9f472a
Rexic <roachl9215@gmail.com>
2022-10-08 19:03:32 +1300

    added different moderation status results to be used to show articles in different areas

---
+++ frontend/src/dummydata/ModerationStatus.js
@@ -0,0 +1,19 @@
+const ModerationStatus = [
+  {
+    id: 1,
+    moderationStatus: 'approved',
+  },
+  {
+    id: 2,
+    moderationStatus: 'denied',
+  },
+  {
+    id: 3,
+    moderationStatus: 'moderationNeeded',
+  },
+  {
+    id: 4,
+    moderationStatus: 'analystNeeded',
+  },
+];
+export default ModerationStatus;
